e09240de45a676462113ce007116695f1a88efc9
napeblossoms <101855341+napeblossoms@users.noreply.github.com>
2022-07-28 23:56:14 +0900

    Add

--- compare.rb
+++ compare.rb
@@ -2,3 +2,19 @@ total = 100
 if total < 200
   puts "合計は200未満です"
 end
+
+dice = 0  # 変数diceに0を代入し、初期値を設定する
+
+while dice != 6 do  # サイコロの目が6ではない間、diceの初期値は0なので条件を満たす。以降はdiceに代入される数によって結果が異なる
+  dice = rand(1..6)  # 1～6の数字がランダムに出力される
+  puts dice
+end
+
+for i in 1..6 do  # "1..6"は、1～6までの範囲を表す
+  puts i
+end
+
+amounts = {"リンゴ"=>2, "イチゴ"=>5, "オレンジ"=>3}
+amounts.each do |fruit, amount|  #ハッシュの内容を順にキーをfruit、値をamountに代入して繰り返す
+  puts "#{fruit}は#{amount}個です。"
+end
